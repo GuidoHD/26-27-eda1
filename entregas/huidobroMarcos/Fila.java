99e2a6756327c78496015b99fda97e1e3730ba60
Marcos <marcoshuidobrodiegorgmail.com>
2026-09-20 22:39:27 +0200

    Añadida base de fila

--- entregas/huidobroMarcos/Fila.java
+++ entregas/huidobroMarcos/Fila.java
@@ -1,5 +1,37 @@
 package huidobroMarcos;
 
 public class Fila {
+
+    public class Fila {
+
+    private Persona[] personas;
+    private int longitud;
+
+    public Fila(int capacidad) {
+        personas = new Persona[capacidad];
+        longitud = 0;
+    }
+
+    public void entrar(Persona persona) {
+        personas[longitud] = persona;
+        longitud++;
+    }
+
+    public Persona salir() {
+        Persona persona = personas[0];
+
+        for (int i = 0; i < longitud - 1; i++) {
+            personas[i] = personas[i + 1];
+        }
+
+        longitud--;
+
+        return persona;
+    }
+
+    public int longitud() {
+        return longitud;
+    }
+}
     
 }
